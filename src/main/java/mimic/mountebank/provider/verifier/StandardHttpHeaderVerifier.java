b5d045fdd2e8bacc71a5280a6522da043fbf585e
Jimmi Kristensen <jikr@tv2.dk>
2019-07-11 10:42:35 +0200

    header verifier can now verify exact http headers

--- src/main/java/mimic/mountebank/provider/verifier/StandardHttpHeaderVerifier.java
+++ src/main/java/mimic/mountebank/provider/verifier/StandardHttpHeaderVerifier.java
@@ -3,10 +3,28 @@ package mimic.mountebank.provider.verifier;
 import mimic.mountebank.imposter.ResponseFields;
 import mimic.mountebank.provider.ProviderResponse;
 
+import java.util.Map;
+
 
 public class StandardHttpHeaderVerifier implements HttpHeaderVerifier {
 
+    @Override
     public Boolean verify(ResponseFields contractResponseFields, ProviderResponse providerResponseFields) {
-        return true;
+        boolean doesStatusMatch = isStatusExactMatch(contractResponseFields.getStatus(), providerResponseFields.getStatus());
+        boolean doesHeadersMatch = isHeadersExactMatch(contractResponseFields.getHeaders(), providerResponseFields.getHeaders());
+
+        return doesStatusMatch && doesHeadersMatch;
     }
+
+    private boolean isStatusExactMatch(int contractStatus, int providerStatus) {
+        if (contractStatus == providerStatus) {
+            return true;
+        }
+        return false;
+    }
+
+    private boolean isHeadersExactMatch(Map<String, String> contractHeaders, Map<String, String> providerHeaders) {
+        return contractHeaders.equals(providerHeaders);
+    }
+
 }
--- src/test/groovy/mimic/provider/HttpHeaderVerifierSpec.groovy
+++ src/test/groovy/mimic/provider/HttpHeaderVerifierSpec.groovy
@@ -11,13 +11,81 @@ class HttpHeaderVerifierSpec extends Specification {
     def "HTTP status code is successfully verified agains imposter response fields"() {
         given:
         def contractResponseFields = new ResponseFields(statusCode: 201)
-        def providerResponseFields = new ProviderResponse(statusCode: 200)
+        def providerResponseFields = new ProviderResponse(statusCode: 201)
 
         when:
         def isVerified = new StandardHttpHeaderVerifier().verify(contractResponseFields, providerResponseFields)
 
         then:
         isVerified == true
+    }
+
+    def "difference in status between contract and provider results in unvarified"() {
+        given:
+        def contractResponseFields = new ResponseFields(statusCode: 201)
+        def providerResponseFields = new ProviderResponse(statusCode: 200)
+
+        when:
+        def isVerified = new StandardHttpHeaderVerifier().verify(contractResponseFields, providerResponseFields)
+
+        then:
+        isVerified == false
+    }
+
+    def "the exact same headers between contract and provider is verified"() {
+        given:
+        def headers = [
+                "X-Header":"this is x",
+                "Y-Header":"this is y"
+        ]
+        def contractResponseFields = new ResponseFields(headers: headers)
+        def providerResponseFields = new ProviderResponse(headers: headers)
+
+        when:
+        def isVerified = new StandardHttpHeaderVerifier().verify(contractResponseFields, providerResponseFields)
+
+        then:
+        isVerified == true
+    }
+
+    def "the exact same headers, but in different order, between contract and provider is verified"() {
+        given:
+        def contractHeaders = [
+                "X-Header":"this is x",
+                "Y-Header":"this is y"
+        ]
+        def providertHeaders = [
+                "Y-Header":"this is y",
+                "X-Header":"this is x"
+        ]
+        def contractResponseFields = new ResponseFields(headers: contractHeaders)
+        def providerResponseFields = new ProviderResponse(headers: providertHeaders)
+
+        when:
+        def isVerified = new StandardHttpHeaderVerifier().verify(contractResponseFields, providerResponseFields)
+
+        then:
+        isVerified == true
+    }
+
+    def "difference in headers between contract and provider is unverified"() {
+        given:
+        def contractHeaders = [
+                "X-Header":"this is x",
+                "Y-Header":"this is y"
+        ]
+        def providertHeaders = [
+                "Z-Header":"this is z",
+                "Y-Header":"this is y"
+        ]
+        def contractResponseFields = new ResponseFields(headers: contractHeaders)
+        def providerResponseFields = new ProviderResponse(headers: providertHeaders)
+
+        when:
+        def isVerified = new StandardHttpHeaderVerifier().verify(contractResponseFields, providerResponseFields)
+
+        then:
+        isVerified == false
     }
 
 }
